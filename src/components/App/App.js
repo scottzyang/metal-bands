f03fff887dad756f3a0b2476104bb3be41ee3648
scottzyang <scottzyang@gmail.com>
2023-02-28 11:52:38 -0600

    Update app and change location to components directory

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Heading from '../Heading/Heading';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
