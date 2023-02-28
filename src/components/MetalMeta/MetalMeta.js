901a77f9630b18b93af96c0e52aab478396a20c8
scottzyang <scottzyang@gmail.com>
2023-02-28 13:21:30 -0600

    Display total fans

--- src/components/MetalMeta/MetalMeta.js
+++ src/components/MetalMeta/MetalMeta.js
@@ -1,10 +1,15 @@
 import data from '../../metal.json'
 
 function MetalMeta() {
+  const total_fans = data.reduce((accumulator, data) => {
+    return accumulator + data.fans;
+  }, 0)
+
   return (
     <section className='flex flex-col items-start m-6'>
       <p><strong>Metal Bands🤘</strong></p>
       <p><strong>Total Bands: </strong>{data.length}</p>
+      <p><strong>Total Number of Fans 🎸: </strong>{total_fans}</p>
     </section>
   )
 }
