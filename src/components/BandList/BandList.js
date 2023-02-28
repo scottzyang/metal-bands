ee97aaa6e7ce79ce5a33adc42bbe27a6df7a4f87
scottzyang <scottzyang@gmail.com>
2023-02-28 14:02:36 -0600

    Add search functionality

--- src/components/BandList/BandList.js
+++ src/components/BandList/BandList.js
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+import './BandList.css'
 import data from '../../metal.json'
 import BandCard from '../BandCard/BandCard'
 
 function BandList() {
-  const bandList = data.map(({ band_name, fans, origin, formed, split, style }) => {
+  // const bandList = data.map(({ band_name, fans, origin, formed, split, style }) => {
+  //   return (
+  //     <div className='border-solid border-4 border-gray-400'>
+  //       <BandCard key={band_name} band_name={band_name} fans={fans} origin={origin} formed={formed} split={split} style={style}/>
+  //     </div>
+  //   )
+  // })
+  const [query, setQuery] = useState('')
+  const bandList = data.filter((obj) => {
+    const inName = obj.band_name.toLowerCase().includes(query.toLowerCase())
+    const inOrigin = obj.origin.toLowerCase().includes(query.toLowerCase())
+    return inName || inOrigin }).map(({ band_name, fans, origin, formed, split, style }) => {
     return (
       <div className='border-solid border-4 border-gray-400'>
         <BandCard key={band_name} band_name={band_name} fans={fans} origin={origin} formed={formed} split={split} style={style}/>
@@ -11,8 +24,22 @@ function BandList() {
   })
 
   return (
-    <section className='grid gap-0 grid-cols-3 w-11/12 m-auto'>
-      {bandList}
+    <section>
+      <section className='flex flex-col items-center mb-8'>
+        <form>
+          <legend className='google-font text-3xl'>Search Band by Name or Country of Origin</legend>
+          <input
+          className='text-slate-800 w-4/5'
+            value={query}
+            placeholder="search"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="ml-4 border-solid border-4 border-white rounded-xl bg-gray-500" type="submit"><p className='m-1'>Submit</p></button>
+        </form>
+      </section>
+      <section className='grid gap-0 grid-cols-3 w-11/12 m-auto'>
+        {bandList}
+      </section>
     </section>
   )
 }
