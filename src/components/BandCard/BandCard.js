a1ef6e3a43b4b9c9676f884dd29f4418298673c9
scottzyang <scottzyang@gmail.com>
2023-02-28 14:07:24 -0600

    Ajust styling of cards

--- src/components/BandCard/BandCard.js
+++ src/components/BandCard/BandCard.js
@@ -8,21 +8,21 @@ function BandCard({band_name, fans, formed, origin, split, style}) {
 
   const band_style = style.split(',').map((style_item) => {
     return (
-      <li className="ml-6 google-font text-m">{style_item}</li>
+      <li className="ml-6 google-font text-lg">{style_item}</li>
     )
   })
 
   return (
     <section>
-      <section className="m-8 flex flex-col items-start">
-        <p className="google-font text-2xl"><strong>Band Name: </strong>{band_name}</p>
-        <p className="google-font text-lg"><strong>Formed: </strong>{formed}</p>
-        <p className="google-font text-lg"><strong>Origin: </strong>{origin}</p>
-        <p className="google-font text-lg"><strong>Fans: </strong>{fans_format(fans)}</p>
-        <p className="google-font text-lg"><strong>Split: </strong>{split}</p>
+      <p className="google-font text-4xl m-8"><strong>Band Name: </strong>{band_name}</p>
+      <section className="m-10 flex flex-col items-start">
+        <p className="google-font text-xl"><strong>Formed: </strong>{formed}</p>
+        <p className="google-font text-xl"><strong>Origin: </strong>{origin}</p>
+        <p className="google-font text-xl"><strong>Fans: </strong>{fans_format(fans)}</p>
+        <p className="google-font text-xl"><strong>Split: </strong>{split}</p>
       </section>
-      <ul className="flex flex-col items-start m-8 list-disc">
-        <p className="google-font text-lg"><strong>Style:</strong></p>
+      <ul className="flex flex-col items-start m-10 list-disc">
+        <p className="google-font text-xl"><strong>Style:</strong></p>
           {band_style}
       </ul>
       <section>
