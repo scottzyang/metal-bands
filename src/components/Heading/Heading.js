71df93860cb030a6a9877cbc1a5834d8bb00971e
scottzyang <scottzyang@gmail.com>
2023-02-28 13:44:29 -0600

    Add keyframes to header image

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -2,10 +2,13 @@ import './Heading.css'
 
 function Heading() {
   return(
-    <header className="flex flex-col items-center m-6">
-      <h1 className="google-font text-5xl">ACS 1320 Final Assessment</h1>
-      <h2 className="google-font text-2xl">Scott Yang</h2>
-    </header>
+    <>
+      <header className="flex flex-col items-center m-6">
+        <h1 className="google-font text-5xl">ACS 1320 Final Assessment</h1>
+        <h2 className="google-font text-2xl">Scott Yang</h2>
+      </header>
+      <img className="header-image" src="https://1035thearrow.com/wp-content/uploads/sites/11/2020/02/GettyImages-1124622103.jpg" alt="james hetfield"/>
+    </>
   )
 }
 
